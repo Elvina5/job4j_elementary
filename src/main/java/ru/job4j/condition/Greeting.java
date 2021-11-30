5c50c5cea35140dd17a5b39aa077df5015482bb0
Manager1 <ely5@mail.ru>
2021-11-30 10:49:33 +0300

    9.1. String [#237407 #234135]

--- src/main/java/ru/job4j/condition/Greeting.java
+++ src/main/java/ru/job4j/condition/Greeting.java
@@ -3,12 +3,7 @@ package ru.job4j.condition;
 public class Greeting {
     public static void main(String[] args) {
         String idea = "I like Java!";
-        String level = "But I am a newbie.";
-        String separate = " ";
-        String first = idea + separate + level;
         int year = 2021;
-        String id = first + separate +  year;
-        System.out.println(id);
-
+        System.out.println(idea + " But I am a newbie. " + year);
     }
 }
