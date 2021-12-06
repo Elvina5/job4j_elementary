aae5e2b4b6247f67cbfb8bf94a6f87b29b58c1a8
Manager1 <ely5@mail.ru>
2021-12-06 21:09:32 +0300

    4.6.1. Switch [#334070]

--- src/main/java/ru/job4j/condition/SwitchWeek.java
+++ src/main/java/ru/job4j/condition/SwitchWeek.java
@@ -1,7 +1,6 @@
 package ru.job4j.condition;
 
 public class SwitchWeek {
-
     public static String nameOfDay(int day) {
         String name;
         switch (day) {
@@ -32,4 +31,5 @@ public class SwitchWeek {
         }
         return name;
     }
+
 }
